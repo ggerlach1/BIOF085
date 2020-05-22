da2d2b257676d4f6804f385d3ed97ffd7107fcf5
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-05-22 02:09:42 -0400

    Updated build_book.R
    Need to use bookdown::gitbook to conform
    to what is written in _output.yml

--- build_book.R
+++ build_book.R
@@ -3,7 +3,7 @@
 library(bookdown)
 
 system('sh _book_prep.sh') # removes potential TOC in the Rmd files
-render_book('index.Rmd', 'gitbook')
+render_book('index.Rmd', 'bookdown::gitbook')
 # render_book('index.Rmd','pdf_book') # PDF version of manual
 
 

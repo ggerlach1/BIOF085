9a068bdc20eab6b0bf01c6da3cf3d541f35c9a19
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-05-22 01:57:22 -0400

    Manual building scripts

--- _book_prep.sh
+++ _book_prep.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-
-for f in dev/0*.Rmd
-do
-  sed '/^</d' $f > ${f/dev/book}
-done
-cp -r dev/data book
-cp -r dev/graphs book
-cd book
-Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
+## This script is targeted at Mac OS where sed -i requires a target
+sed -i '' '/^</d' *.Rmd
---
+++ build_book.R
@@ -0,0 +1,18 @@
+# R script for building manual using bookdown
+
+library(bookdown)
+
+system('sh _book_prep.sh') # removes potential TOC in the Rmd files
+render_book('index.Rmd', 'gitbook')
+# render_book('index.Rmd','pdf_book') # PDF version of manual
+
+
+# NOTE: There is an issue with reticulate 1.15, Mac OS 10.13 and greater, 
+# SIP in the Mac OS, and the notarized version of R, which is preventing 
+# us from running the reticulate python interpretor using Rscript or from the
+# command line or from R running in a terminal. This compilation can currently
+# only be done from within RStudio. 
+# 
+# See https://github.com/rstudio/reticulate/issues/758
+# 
+# Otherwise writing a Makefile or a shell script would be a better method. 
